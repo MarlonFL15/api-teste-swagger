7fc7e17b6affd779ddccdc0e9f690fc907f73e4e
unknown <marlonfleite50@gmail.com>
2021-12-12 20:38:59 -0400

    implementando o swagger

--- app/app.js
+++ app/app.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocs = require('./swagger.json')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 app.use('/', routes)
+app.use(cors())
+app.get('/termos', (req, res) => res.send('Termos'))
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const blogController = require('./controllers/blogController')
 
-router.get('/blogs', blogController.findAll);
-router.get('/blog/:blogId', blogController.findById)
-router.post('/blog', blogController.insert)
-router.put('/blog/:blogId', blogController.update)
+router.get('/blogs', blogController.findAll); //ok
+router.get('/blog/:blogId', blogController.findById) //ok
+router.post('/blog', blogController.insert) //ok
+router.put('/blog/:blogId', blogController.update) //ok
 router.delete('/blog/:blogId', blogController.deleteById)
-router.delete('/blogs', blogController.deleteAll)
+router.delete('/blogs', blogController.deleteAll) //ok
 
 module.exports = router;
